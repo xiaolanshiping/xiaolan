ff521f49bf6a99346538746d6634616c6e741893
xiaolanshiping <xiaolanshipinsan@gmail.com>
2025-12-18 14:28:30 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,91 +1,66 @@
+// ======================= 子域名随机生成逻辑 ===========================
 var subdomains = [
-    'thu',  // 清华大学 (Tsinghua University)
-    'pku',  // 北京大学 (Peking University)
-    'fdu',  // 复旦大学 (Fudan University)
-    'sjtu', // 上海交通大学 (Shanghai Jiao Tong University)
-    'zju',  // 浙江大学 (Zhejiang University)
-    'nju',  // 南京大学 (Nanjing University)
-    'xmu',  // 厦门大学 (Xiamen University)
-    'sysu', // 中山大学 (Sun Yat-sen University)
-    'whu',  // 武汉大学 (Wuhan University)
-    'jlu',  // 吉林大学 (Jilin University)
-    'scu',  // 四川大学 (Sichuan University)
-    'ruc',  // 中国人民大学 (Renmin University of China)
-    'bnu',  // 北京师范大学 (Beijing Normal University)
-    'nku',  // 南开大学 (Nankai University)
-    'tju',  // 同济大学 (Tongji University)
-    'sdu',  // 山东大学 (Shandong University)
-    'hust', // 华中科技大学 (Huazhong University of Science and Technology)
-    'xjtu',  // 西安交通大学 (Xi'an Jiaotong University)
-    'hit',  // 哈尔滨工业大学 Harbin Institute of Technology
+    'thu', 'pku', 'fdu', 'sjtu', 'zju', 'nju', 'xmu', 'sysu', 'whu', 'jlu',
+    'scu', 'ruc', 'bnu', 'nku', 'tju', 'sdu', 'hust', 'xjtu', 'hit'
 ];
+
 function getRandomSubdomain() {
-    var index = Math.floor(Math.random() * subdomains.length);
-    return subdomains[index];
+    return subdomains[Math.floor(Math.random() * subdomains.length)];
 }
 
-function getRandomString() {
+function getRandomString(length = 3) {
     var result = '';
     var characters = 'abcdefghijklmnopqrstuvwxyz';
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
 }
 
-var emails = ['hlqbj2025@gmail.com'];
+function randomNum(minNum, maxNum) {
+    return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+}
 
-var urls = [];
-urls.push(".eqihorut.xyz");
+// ======================= 地址配置 ===========================
 
-// ★ 必须先定义 JumpPage
-var JumpPage = "https://hlqbj.com";
+var baseDomains = [
+    'qlqcyqm.cc',
+];
 
-// ★ 修复成无语法错误 + 自动去重
-var foreverUrls = Array.from(new Set([
-    'https://xiaolan2.com',
-    JumpPage,
-    'https://xiaolan.co'
-]));
+var emails = ['91dsp2008@gmail.com'];
+
+var JumpPage = "https://91zuo.com";
 
 var newestUrls = [];
-
-for (var i = 0; i < urls.length * 3; i++) {
-    newestUrls.push('https://' + getRandomSubdomain() + urls[randomNum(0, urls.length - 1)]);
+for (var i = 0; i < baseDomains.length * 3; i++) {
+    newestUrls.push('https://' + getRandomSubdomain() + '.' + baseDomains[randomNum(0, baseDomains.length - 1)]);
 }
 
 var otherUrls = [
-    'https://xiaolan.github.io'
+    'https://91zuo.github.io',
+    'https://91zuo.pages.dev',
+    'https://91zuo.com'
 ];
+
+var foreverUrls = ['https://91zuo.com', JumpPage];
 
 var notices = [
-    '* 我们推荐PC和Andriod手机用户使用Chrome(谷歌)浏览器访问，iPhone用户建议使用自带Safari浏览器。',
-    '* 大陆地区用户建议使用VPN或代理访问永久地址。',
-    '* 大陆地区用户强烈建议截图保存当前页面。'
+    '* 我们推荐 PC 和 Android 手机用户使用 Chrome（谷歌）浏览器访问，iPhone 用户建议使用 Safari 浏览器。',
+    '* 大陆地区用户建议使用 VPN 或代理访问永久地址。',
+    '* 强烈建议截图收藏当前页面，以防迷路。'
 ];
 
-// 随机数函数
-function randomNum(minNum, maxNum) {
-    switch (arguments.length) {
-        case 1:
-            return parseInt(Math.random() * minNum + 1, 10);
-        case 2:
-            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
-        default:
-            return 0;
-    }
-}
+// ======================= 页面生成函数 ===========================
 
 function createFieldElem(option) {
     var title = option.title;
-    var items = option.items;
+    var items = option.items || [];
     var plainText = option.plainText;
-    var classStr = option.classStr;
+    var classStr = option.classStr || '';
     var text = option.text;
 
     var fieldElem = document.createElement('div');
-    var fieldClass = ['field', classStr].join(' ');
-    fieldElem.setAttribute('class', fieldClass);
+    fieldElem.setAttribute('class', `field ${classStr}`.trim());
 
     var titleElem = document.createElement('h4');
     titleElem.setAttribute('class', 'title');
@@ -94,14 +69,11 @@ function createFieldElem(option) {
 
     var ulElem = document.createElement('ul');
     var htmlStr = '';
-
-    if (items && items.length > 0) {
-        for (var i = 0; i < items.length; i++) {
-            if (plainText) {
-                htmlStr += '<li>' + items[i] + '</li>';
-            } else {
-                htmlStr += '<li><a href="' + items[i] + '" target="_blank">' + items[i] + '</a></li>';
-            }
+    for (var i = 0; i < items.length; i++) {
+        if (plainText) {
+            htmlStr += '<li>' + items[i] + '</li>';
+        } else {
+            htmlStr += '<li><a href="' + items[i] + '" target="_blank" rel="noopener noreferrer">' + items[i] + '</a></li>';
         }
     }
 
@@ -111,38 +83,41 @@ function createFieldElem(option) {
 
     ulElem.innerHTML = htmlStr;
     fieldElem.appendChild(ulElem);
-
     return fieldElem;
 }
+
+// ======================= 页面加载 ===========================
 
 window.onload = function () {
     var mainElem = document.getElementById('main');
 
+    // logo
     var logoElem = document.createElement('div');
     logoElem.setAttribute('class', 'brand');
     logoElem.setAttribute('id', 'logo');
-    logoElem.innerHTML = '<img src="logo.png" width="180"/>';
+    logoElem.innerHTML = '<img src="logo.png" width="180" style="margin-bottom: 10px;" />';
     mainElem.appendChild(logoElem);
 
-    var newFavoritesElem = createFieldElem({
-        title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路',
-        items: ""
-    });
-    mainElem.appendChild(newFavoritesElem);
+    // 收藏提示
+    var favElem = createFieldElem({ title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: [] });
+    mainElem.appendChild(favElem);
 
+    // 最新地址
     var newestFieldElem = createFieldElem({
-        title: '最新地址',
+        title: '最新可用地址',
         items: newestUrls,
-        text: '请使用 https:// 协议访问小蓝视频官网'
+        text: '请使用 https 协议访问本站。'
     });
     mainElem.appendChild(newestFieldElem);
 
+    // 备用发布页
     var otherFieldElem = createFieldElem({
-        title: '由于风控原因，网址经常被墙<br/>务必保存下面所有地址发布页',
+        title: '地址发布页备份（请全部收藏）',
         items: otherUrls
     });
     mainElem.appendChild(otherFieldElem);
 
+    // 获取地址邮箱
     var mailFieldElem = createFieldElem({
         title: '发送邮件获得最新地址',
         items: emails,
@@ -150,12 +125,14 @@ window.onload = function () {
     });
     mainElem.appendChild(mailFieldElem);
 
+    // 永久地址
     var foreverFieldElem = createFieldElem({
         title: '永久地址',
         items: foreverUrls
     });
     mainElem.appendChild(foreverFieldElem);
 
+    // 注意事项
     var noticeFieldElem = createFieldElem({
         title: '注意事项',
         items: notices,
@@ -163,8 +140,4 @@ window.onload = function () {
         classStr: 'desc'
     });
     mainElem.appendChild(noticeFieldElem);
-}
-
-
-
-
+};
